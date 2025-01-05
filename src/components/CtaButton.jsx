f9e3e2ed449ef1b49baf879687600aa265476eee
sherifsani <sanisherif11@gmail.com>
2025-01-05 17:42:01 +0100

    completed hero section

--- src/components/CtaButton.jsx
+++ src/components/CtaButton.jsx
@@ -2,20 +2,25 @@
 const CtaButton = ({ text }) => {
   return (
     <div className="">
-      <a href="" className="btn bg-[#222] text-[hsl(0,0%,100%)] rounded-3xl ">
+      <a
+        href=""
+        className="cta-btn bg-[#222] text-[hsl(0,0%,100%)] rounded-full  font-[500]"
+      >
         {text}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="rounded-full bg-[#d1c9ff]"
-        >
-          <path
-            d="m11.293 17.293 1.414 1.414L19.414 12l-6.707-6.707-1.414 1.414L15.586 11H6v2h9.586z"
-            fill="#222"
-          />
-        </svg>
+        <span className="btn-arrow rounded-full w-[40px] h-[40px] bg-[#d1c9ff]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className=""
+          >
+            <path
+              d="m11.293 17.293 1.414 1.414L19.414 12l-6.707-6.707-1.414 1.414L15.586 11H6v2h9.586z"
+              fill="#222"
+            />
+          </svg>
+        </span>
       </a>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import CtaButton from "./CtaButton";
 
 const Hero = () => {
   return (
-    <section className="flex h-[100vh] flex-col pt-[80px] pb-[30px] gap-7 px-5 justify-end mx-auto mx-w-[1400px] border-b">
-      <h1 className="hero-head text-[15.3vw] leading-[60px] tracking-[-0.9px]">
+    <section className="flex h-[100vh] flex-col items-start justify-center pt-[100px] pb-[30px] gap-7 px-5 mx-auto mx-w-[1400px] border-b">
+      <h1 className="hero-head text-[15.3vw] sm:text-[60px] lg:text-[72px] xl:text-[112px]  tracking-[-0.9px] lg:tracking-[-1.68px]">
         Your Ultimate Fashion Destination
       </h1>
-      <p className="hero-text">
+      <p className="hero-text max-w-[400px]">
         Discover trends, embrace elegance, and redefine your wardrobe with us.
       </p>
 
